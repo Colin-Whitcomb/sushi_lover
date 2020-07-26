f86509239f86ec1c427fb0d90c37e9f1fdf1a371
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-25 20:39:48 -0700

    Added update model code.

--- models/sushi.js
+++ models/sushi.js
@@ -2,16 +2,23 @@ var orm = require("../config/orm.js");
 
 
 var sushi = {
-    all: function(cb) {
-        orm.all("sushi", function(res) {
-            cb(res)
-        })
-    },
+  all: function (cb) {
+    orm.all("sushi", function (res) {
+      cb(res)
+    })
+  },
 
-    // The variables cols and vals are arrays.
-  create: function(name, cb) {
-                // "sushi_name" "devoured"   
-    orm.create("sushi", ["sushi_name", "devoured"],[name, 0], function(res) {
+  // The variables cols and vals are arrays.
+  create: function (name, cb) {
+    // "sushi_name" "devoured"   
+    orm.create("sushi", ["sushi_name", "devoured"], [name, 0], function (res) {
+      cb(res);
+    });
+  },
+
+  update: function (id, cb) {
+    // send ORM table name and id of obj
+    orm.update("sushi", id, function (res) {
       cb(res);
     });
   },
